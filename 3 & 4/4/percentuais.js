f45913db50d058cec1387e05de70712af230a2fb
caesar <joaovictorcsatu@gmail.com>
2022-06-15 00:05:34 -0300

    update

--- 3 & 4/4/percentuais.js
+++ 3 & 4/4/percentuais.js
@@ -1,20 +1,27 @@
 const { dataArray } = require("./dados_estaduais");
 
-console.log(dataArray);
-
 let auxArray = [];
 
 for (let index = 0; index < dataArray.length; index++) {
   auxArray.push(dataArray[index].valor);
-  console.log(auxArray);
 }
 
 let sum = auxArray.reduce(function (a, b) {
   return a + b;
 });
 
-auxArray.forEach((valor) => {
-  Math.round(valor);
+for (let index = 0; index < dataArray.length; index++) {
+  let valor = dataArray[index].valor;
   let aux = (valor / sum) * 100;
-  console.log(`O valor ${valor} corresponde a ${Math.round(aux)}% de ${sum}`);
-});
+
+  console.log(
+    `O valor ${valor} do estado ${
+      dataArray[index].estado
+    } corresponde a ${Math.round(aux)}% de ${sum}`
+  );
+}
+
+// auxArray.forEach((valor) => {
+//   Math.round(valor);
+
+// });
